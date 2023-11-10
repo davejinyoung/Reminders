81d83d80e6a127e983a06e0b1b0cf6cba00f6bf5
Dave Jung <davejinyoung@uvic.ca>
2023-11-09 22:21:12 -0800

    Added ReminderDateTime class to make reminders app.

---
+++ src/reminder_datetime.py
@@ -0,0 +1,12 @@
+from datetime import *
+
+class ReminderDateTime:
+
+    def __init__(self):
+        self.year = 0
+        self.month = 0
+        self.day = 0
+        self.hour = 0
+        self.min = 0
+        self.sec = 0
+
